2a60530b37ab3fc4773b6b37c92199e9dc29bc0c
Matthew Malcomson <hardenedapple@gmail.com>
2017-05-20 18:01:23 +0100

    Play around with reading the system memory map
    Don't actually do anything with it as yet.
    Also have removed old files that were cluttering the repo up.

--- checkstate.asm
+++ checkstate.asm
@@ -10,6 +10,70 @@ readMemorySize:
     #
     # In newer BIOSes, get system memory map
     # vimcmd: e +69 saved_docs/BIOSinterrupts/INTERRUP.D
+    # NOTE:
+    #   The description linked above says the two ways that BIOS's are
+    #   permitted to indicate the end of the list are to return %ebx with $0 or
+    #   to return non-zero %ebx and set the CF on the next iteration.
+    #       I don't account for the second option.
+    # Magic numbers.
+    movl $0x534D4150, %edx
+    # Continuation value (see the BIOS interrupt link above).
+    movl $0, %ebx
+loop_again:
+    movl $0x0000e820, %eax
+    # Size of buffer for result.
+    movl $(memmap_end - memmap_start), %ecx
+    # Location of buffer (assuming %es is at 0).
+    movw $memmap_start, %di
+    int $0x15
+    jnc 1f
+    ERRMSG(msgMemSizeFailed)
+    jmp .
+1:
+    cmpl $0x534D4150, %eax
+    je 1f
+    ERRMSG(msgMemSizeFailed)
+    jmp .
+1:
+    cmpl $20, %ecx
+    je 1f
+    ERRMSG(msgMemSizeFailed)
+    jmp .
+1:
+    pushl %ebx
+    movw $memmap_start, %ax
+    call printQWORD
+    movw $(memmap_start + 8), %ax
+    call printQWORD
+    movw $(memmap_start + 0x10), %ax
+    call printDWORD
+    call newline
+
+    popl %ebx
+    cmpl $0, %ebx
+    je memmapexit
+    jmp loop_again
+memmapexit:
+    ret
+    # Print out information about the system map
+    # Not really much else interesting to do, we're not loading anything as yet.
+
+
 getAvailableVideoModes:
     # Have no idea how to do this at the moment, but the
     # Rolling_Your_Own_Bootloader wiki page says it's easier in Real mode.
+    #
+
+datastart
+# Buffer for memory map, load one entry at a time.
+# Loading one entry at a time to fully exercise the iteration code, once that
+# seems to be working fine, I'll go on to loading 2 at a time to make sure the
+# counter increments correctly before finally loading however many entries my
+# machine returns at a time to make sure the edge case of no iteration is
+# handled correctly too.
+msgMemSizeFailed: .asciz "getting memory size failed"
+memmap_start:
+    # .skip 100, 0
+    .skip 20, 0
+memmap_end:
+dataend
--- printing.asm
+++ printing.asm
@@ -26,8 +26,24 @@ BIOSprint:
     cmpb $0, %al
     jne 1b
     ret
-    
+
+newline:
+    movw $0x1, %bx
+    movb $0xe, %ah
+    movb $0xa, %al
+    int $0x10
+    movb $0xd, %al
+    int $0x10
+    ret
+
 printReg:
+    # Argument to this function -- %al marks whether to terminate with a
+    # newline.
+    cmpb $0, %al
+    je 1f
+    movb $0xa, %al
+1:
+    movb %al, outstr16 + 4
     movw $outstr16, %di
     movw reg16, %ax
     movw $hexstr, %si
@@ -46,8 +62,39 @@ hexloop:
     printString($outstr16)
     ret
 
+printDWORD:
+    movw %ax, storedaddr
+    movw $1, %cx
+    jmp 1f
+printQWORD:
+    # Address of qword is in %ax
+    movw %ax, storedaddr
+    movw $3, %cx
+1:
+    movw storedaddr, %si
+    movw %cx, %ax
+    shlw $1, %ax
+    addw %ax, %si
+    movw (%si), %ax
+    movw %ax, reg16
+    pushw %cx
+    movb $0, %al
+    call printReg
+    popw %cx
+    loop 1b
+    # Finish, have a new line and return.
+printQWORDfin:
+    movw storedaddr, %si
+    movw (%si), %ax
+    movw %ax, reg16
+    movb $1, %al
+    call printReg
+    ret
+
+
 datastart
 hexstr:     .ascii "0123456789ABCDEF"
 outstr16:   .byte 0, 0, 0, 0, 0xa, 0xd, 0
 reg16:      .word 0
+storedaddr: .word 0
 dataend
